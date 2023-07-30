b61512620c5d3adac9748e33cc3add3d11863176
FlyingCat <41324945+FlyingCat-fa@users.noreply.github.com>
2023-07-30 12:46:18 +0800

    Add files via upload

--- medical_dict/dict.py
+++ medical_dict/dict.py
@@ -1,15 +1,9 @@
 import os
 from sys import path
 path.append(os.getcwd())
-root_path = '/home/Medical_Understanding'
-path.append(root_path)
+# path.append('/home/Medical_Understanding/TSPMN')
 
-from common_utils import write_txt, read_txt, read_json, write_json
-
-# data_path = 'data_chunyu.json'
-# data_100_path = 'data_chunyu_100.json'
-# data = read_json(data_path)
-# write_json(data=data[:100], path=data_100_path)
+from common_utils import write_txt, read_json, write_json, read_scel
 
 def read_word_from_graph(path):
     graph = read_json(path=path)
@@ -20,7 +14,7 @@ def read_word_from_graph(path):
     words = list(set(words))
     return words
 
-def read_txt_v2(path):
+def read_txt(path):
     lines = []
     with open(path, "r", encoding="utf-8") as f:
         file = f.readlines()
@@ -29,17 +23,13 @@ def read_txt_v2(path):
     return lines
 
 path_1 = 'medical_dict/THUOCL_medical.txt'
-dict_1 = read_txt_v2(path_1)
+dict_1 = read_txt(path_1)
 
-path_2 = 'medical_dict/medical_words.txt'
-dict_2 = read_txt(path=path_2)
+path_2 = 'medical_dict/sougou_dict.scel'
+dict_2 = read_scel(os.path.join(path_2))
 
-# graph_pathes = ['VRBot-sigir2021-datasets/kamed_joint_graph/graph.json', 
-#                 'VRBot-sigir2021-datasets/meddg_joint_graph/graph.json', 
-#                 'VRBot-sigir2021-datasets/meddialog_joint_graph/graph.json']
-
-graph_pathes = ['VRBot-sigir2021-datasets/kamed_joint_graph/graph.json', 
-                'VRBot-sigir2021-datasets/meddialog_joint_graph/graph.json']
+graph_pathes = ['data4pretrain/VRBot-sigir2021-datasets/kamed_joint_graph/graph.json', 
+                'data4pretrain/VRBot-sigir2021-datasets/meddialog_joint_graph/graph.json']
 
 graph_words = []
 for path in graph_pathes:
@@ -55,7 +45,7 @@ all_words.extend(dict_2)
 all_words = list(set(all_words))
 single_words = []
 for word in all_words:
-    if len(word) == 1:
+    if len(word) == 1: # 去掉单字
         single_words.append(word)
 
 all_words = list(set(all_words) - set(single_words))
